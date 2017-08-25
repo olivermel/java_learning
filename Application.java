7da56f90fbe669cb3077805defb9d868c6ff05a0
root <root@centos-7.olivermel.com>
2017-08-25 19:22:57 +0000

    my first java code

---
+++ Application.java
@@ -0,0 +1,7 @@
+class Application
+{
+   public static void main (String[] args)
+   {
+	System.out.println("Omo ehn i don tire but na me and this java " + args[0]); 
+   }
+}
---
+++ javapp.java
@@ -0,0 +1,27 @@
+public class javapp {
+  public static void main(String[] args ) {
+
+	int 	myNumber = 10;
+	short	myShort	 = 888;
+	long	myLong	 = 88888;
+	
+	double	myDouble = 9.999;
+	float	myFloat  = 999.9f;
+	
+	char	myChar	 = '!';
+	boolean	myBoolean = true;
+
+	byte	myByte	 = 127;
+	
+
+	System.out.println(myNumber);
+	System.out.println(myShort);
+	System.out.println(myLong);
+	System.out.println(myDouble);
+	System.out.println(myFloat);
+	System.out.println(myChar);
+	System.out.println(myBoolean);
+	System.out.println(myByte);
+
+  }
+} 
