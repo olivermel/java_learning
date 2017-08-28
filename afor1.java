43eb1932914dba9842451517a5e5563a963cf30f
Olivermel <Olivermel@yahoo.com>
2017-08-28 20:15:49 +0000

    added the line break

--- afor1.java
+++ afor1.java
@@ -3,7 +3,7 @@ public class afor1 {
    public static void main(String[] args) {
 
 	for(int i=0; i < 5; i = i + 1 ) {
-	   System.out.printf("The value of i is: %d", i);
+	   System.out.printf("The value of i is: %d\n", i);
 
 	}
 
