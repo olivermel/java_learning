7794321f1bc09abd29350c027c700ed4770f664a
Olivermel <Olivermel@yahoo.com>
2017-08-29 15:00:13 +0000

    infinite loop with while and how to comment java line out

---
+++ infiniteloop.java
@@ -0,0 +1,14 @@
+public class infiniteloop {
+
+   public static void main(String[] arg){
+
+
+	int loop = 0;
+
+	while(loop < 5) {
+	   System.out.println("looping: " + loop);
+
+	   // loop++;
+	}
+   }
+}
