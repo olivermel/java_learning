45beeb420ed61731a717f8117d6fbe29f758114f
Olivermel <Olivermel@yahoo.com>
2017-08-29 14:51:04 +0000

    looking at if statements

---
+++ ifapp.java
@@ -0,0 +1,22 @@
+public class ifapp {
+
+   public static void main(String[] arg ) {
+
+     int myInt = 15;
+
+     if(myInt < 10) {
+	System.out.println("Yes this is true");
+     }
+     
+
+     else if(myInt > 20) {
+	System.out.println("No this is false");
+     }
+    
+
+     else {
+  	System.out.println("None of the above");
+     }
+
+   }
+}  
