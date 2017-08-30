9ff0b691cd81de2735f20cc2d493f25f5c76c3ec
Olivermel <Olivermel@yahoo.com>
2017-08-30 13:33:45 +0000

    floating numbers

---
+++ floats.java
@@ -0,0 +1,15 @@
+public class floats {
+
+   public static void main(String[] args) {
+
+	float first_number, second_number, answer;
+	
+	first_number = 10.5f;
+	second_number = 20.8f;
+	answer = first_number / second_number;
+
+	System.out.println("Total = " + answer );
+
+   }
+
+}
