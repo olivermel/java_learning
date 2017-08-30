81a937ddc20018c1d5553eeec002d2296a958163
Olivermel <Olivermel@yahoo.com>
2017-08-30 17:53:10 +0000

    working with multiple string

---
+++ stringing.java
@@ -0,0 +1,17 @@
+public class stringing {
+
+   public static void main(String[] arg) {
+
+	//string first_name, middle_name, last_name, complete_name;
+
+	String first_name  = "Melville";
+	String middle_name = "Ikenna";
+	String last_name   = "Ezeogu";
+	//complete_name = (first_name + middle_name + last_name);
+
+
+	System.out.println(first_name + " " + middle_name + " " + last_name);
+  
+   }
+
+}
