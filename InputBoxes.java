f13c73818c40daa29be8ec9024215d725fc75b12
Olivermel <Olivermel@yahoo.com>
2017-08-30 18:24:21 +0000

    more input boxes stuff

--- InputBoxes.java
+++ InputBoxes.java
@@ -11,15 +11,19 @@ public class InputBoxes
   {
 
 	String first_name;
-	first_name = JOptionPane.showInputDialog("First Name");
+	first_name = JOptionPane.showInputDialog("First Name", "Enter your First Name");
 	
 	String family_name;
-	family_name = JOptionPane.showInputDialog("Family Name");
+	family_name = JOptionPane.showInputDialog("Last Name", "Enter your Family Name");
 
 	String full_name;
 	full_name = "You are " + first_name + " " + family_name;
 
-	JOptionPane.showMessageDialog(null, full_name);
+	//JOptionPane.showMessageDialog(null, full_name);
+	JOptionPane.showMessageDialog(null, full_name, "Name", JOptionPane.INFORMATION_MESSAGE);
+
+
+
 	
 	System.exit(0);	
  
