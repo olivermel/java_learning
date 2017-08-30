329e6ed2f9f83a155f0bae2b9dccc942d001cf98
Olivermel <Olivermel@yahoo.com>
2017-08-30 14:47:32 +0000

    additions in java

---
+++ additionss.java
@@ -0,0 +1,16 @@
+public class additionss {
+
+   public static void main(String[] args) {
+
+	int first_number, second_number, third_number, answer;
+
+	first_number	= 10;
+	second_number	= 20;
+	third_number	= 5;
+	answer		= second_number - (first_number + third_number);
+
+	System.out.println("Total is = " + answer );
+
+   }
+
+}
